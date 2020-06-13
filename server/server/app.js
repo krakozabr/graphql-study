51b8f6523f37d1354709c6bae105ae65c4106367
Maksym <gontcharuk@gmail.com>
2020-06-13 09:16:06 +0200

    #11 connecting front-end & back-end

--- server/server/app.js
+++ server/server/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('../schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3005;
 
 mongoose.connect('mongodb+srv://maks_go:12345@graph-vydw2.mongodb.net/graph?retryWrites=true&w=majority', { useNewUrlParser: true });
 
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema, 
